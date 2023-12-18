148bcd66915e1e95afd4f8dc71f32e6b5876e5bc
hassanscript <hsn24sk@gmail.com>
2023-12-18 21:14:26 +0500

    010

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
-    <div>
+    <div className="flex flex-col h-screen">
       <Header />
+      <main className="flex-1"></main>
+      <Footer />
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,37 @@
 export const Header = () => {
-  return <header>I am the header</header>;
+  return (
+    <header className="bg-slate-700 p-10 py-4 flex justify-between items-center text-white">
+      <div>
+        <h1 className="text-xl font-bold">HANGMAN!</h1>
+      </div>
+      <nav>
+        <ul className="flex gap-10">
+          <li>
+            <button
+              className="uppercase p-4 py-1 bg-white text-slate-700 rounded-md font-bold block
+            hover:bg-slate-300 hover:scale-110 transition-all"
+            >
+              ANIMALS
+            </button>
+          </li>
+          <li>
+            <button
+              className="uppercase p-4 py-1 bg-white text-slate-700 rounded-md font-bold block
+            hover:bg-slate-300 hover:scale-110 transition-all"
+            >
+              COUNTRIES
+            </button>
+          </li>
+          <li>
+            <button
+              className="uppercase p-4 py-1 bg-white text-slate-700 rounded-md font-bold block
+            hover:bg-slate-300 hover:scale-110 transition-all"
+            >
+              FRUITS
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
