85d9c58520a0bb3ea015a1aa4fc1c0b7f3cf1190
hassanscript <hsn24sk@gmail.com>
2023-12-19 23:29:34 +0500

    012

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,13 @@
 export const Footer = () => {
-  return <footer>I am the footer</footer>;
+  return (
+    <footer className="flex justify-between items-center p-10 py-1 bg-slate-700 text-white text-sm">
+      <p>Hangman. Copyright 2023</p>
+      <p>
+        Created by{" "}
+        <a href="#" className="text-blue-200 underline">
+          Hassan
+        </a>
+      </p>
+    </footer>
+  );
 };
